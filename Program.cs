988a51cf70127cacfb9fec6acbd8f8aad08bc076
ShahnawazMohsin2022 <mohsin.shahnawaz@gmail.com>
2025-05-11 04:10:27 +0530

    updated CsProj and Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello from MyFirstApp on Azure App Service!");
+
+app.Run();
